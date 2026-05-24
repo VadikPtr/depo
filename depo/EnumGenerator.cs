f88a8e251b856773f35909a952356cb874494328
VadikPtr <va00iosd@gmail.com>
2026-05-24 19:38:22 +0500

    enum generator priority

--- depo/EnumGenerator.cs
+++ depo/EnumGenerator.cs
@@ -2,16 +2,18 @@
 
 namespace depo;
 
-internal static partial class EnumGenerator {
+internal static class EnumGenerator {
+  private record EnumValue(int priority, string name);
+
   internal static void generate(GenerateEnum generate_enum, HashSet<string> files) {
-    SortedSet<string> enum_keys = extract_enum_keys(generate_enum.macro_name, files);
-    string            enum_name = get_enum_name(generate_enum.macro_name);
-    string            output    = generate_file_content(enum_name, enum_keys);
+    EnumValue[] enum_keys = extract_enum_keys(generate_enum.macro_name, files);
+    string      enum_name = get_enum_name(generate_enum.macro_name);
+    string      output    = generate_file_content(enum_name, enum_keys);
     write_content(output, generate_enum.out_path);
   }
 
-  private static SortedSet<string> extract_enum_keys(string macro_name, HashSet<string> files) {
-    SortedSet<string> enums_keys = [];
+  private static EnumValue[] extract_enum_keys(string macro_name, HashSet<string> files) {
+    List<EnumValue> enums_keys = [];
     foreach (var file in files) {
       string          text    = File.ReadAllText(file);
       MatchCollection matches = Regex.Matches(text, $@"{macro_name}\(([^)]*)\)");
@@ -20,10 +22,20 @@ internal static partial class EnumGenerator {
         if (value.Contains('.')) {
           continue;
         }
-        enums_keys.Add(value);
+        int priority = 0;
+        if (value.Contains(',')) {
+          var values = value.Split(',');
+          if (values.Length > 1 && int.TryParse(values[1], out priority)) {
+            value = values[0].Trim();
+          }
+        }
+        enums_keys.Add(new EnumValue(priority, value));
       }
     }
-    return enums_keys;
+    return enums_keys.DistinctBy(x => x.name)
+      .OrderByDescending(x => x.priority)
+      .ThenBy(x => x.name)
+      .ToArray();
   }
 
   private static void write_content(string output, string out_path) {
@@ -36,9 +48,15 @@ internal static partial class EnumGenerator {
     File.WriteAllText(out_path, output);
   }
 
-  private static string generate_file_content(string enum_name, SortedSet<string> enum_keys) {
-    var enum_values   = string.Join('\n', enum_keys.Select((x,    i) => $"  {x} = {i},"));
-    var x_enum_values = string.Join(" \\\n", enum_keys.Select((x, i) => $"  X({x})"));
+  private static string generate_file_content(string enum_name, EnumValue[] enum_keys) {
+    var enum_values = string.Join('\n', enum_keys.Select((x, i) => {
+      var priority_string = "";
+      if (x.priority != 0) {
+        priority_string = $" /* priority:{x.priority} */";
+      }
+      return $"  {x.name} = {i},{priority_string}";
+    }));
+    var x_enum_values = string.Join(" \\\n", enum_keys.Select(x => $"  X({x.name})"));
     return
       $$"""
       #pragma once
@@ -47,7 +65,7 @@ internal static partial class EnumGenerator {
       {{enum_values}}
       };
 
-      inline constexpr size_t {{enum_name}}Count = {{enum_keys.Count}};
+      inline constexpr size_t {{enum_name}}Count = {{enum_keys.Length}};
 
       #define x{{enum_name}}(X) \
       {{x_enum_values}}
